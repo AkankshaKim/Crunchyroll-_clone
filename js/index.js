32eedec9a73b24da8d97a5c1787df0403ef42789
AkankshaKim <akankshapadave3@gmail.com>
2023-08-28 16:07:35 +0530

    networking class added

--- js/index.js
+++ js/index.js
@@ -1,90 +1,22 @@
-// Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.2.0/firebase-app.js";
-import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.2.0/firebase-auth.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { networking } from "./networking.js";
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyC7PqXixX7SlpOwzLNPOzy4Rm-HvBmHSl8",
-  authDomain: "crunchyroll-clone-c8501.firebaseapp.com",
-  projectId: "crunchyroll-clone-c8501",
-  storageBucket: "crunchyroll-clone-c8501.appspot.com",
-  messagingSenderId: "938254019867",
-  appId: "1:938254019867:web:726750583cc44213ccb7be"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-const siginbtn = document.getElementById("siginbtn");
+const nt = new networking();
 
 
-
-const loginbtn = document.getElementById("login");
 try{
+
+  const loginbtn = document.getElementById("login");
   loginbtn.addEventListener("click",()=>{
-      checkuser();
+    var email = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    nt.checkuser(email,password);
       
   });
 }
 catch(e){
   siginbtn.addEventListener("click",()=>{
-    createuser();
-  });
-}
-
-
-
-
-
-const checkuser = ()=>{
     var email = document.getElementById("username").value;
     var password = document.getElementById("password").value;
-    const auth = getAuth();
-    signInWithEmailAndPassword(auth, email, password).then((userCredential) => {
-    // Signed in 
-    const user = userCredential.user;
-    alert("everthing good");
-    // ...
-  }).catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    if(errorCode == "auth/network-request-failed")
-    {
-      alert("check your internet connection");
-    }
-    else
-    {
-      alert("check your email or password");
-
-    }
-    
-  });
-}
-
-
-const createuser = ()=>{
-  const auth = getAuth();
-  var email = document.getElementById("username").value;
-  var password = document.getElementById("password").value;
-  createUserWithEmailAndPassword(auth, email, password).then((userCredential) => {
-    // Signed in 
-    const user = userCredential.user;
-    alert("account created");
-    // ...
-  }).catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    if(errorCode == "auth/email-already-in-use")
-    {
-      alert("useralready exists");
-    }
-    else if(errorCode == "auth/network-request-failed")
-    {
-      alert("check your internet connection");
-    }
-    // ..
+    nt.createuser(email,password);
   });
 }
---
+++ js/networking.js
@@ -0,0 +1,66 @@
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.2.0/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.2.0/firebase-auth.js";
+
+class networking{
+    constructor()
+    {
+        this.appconfig = {
+            apiKey: "AIzaSyC7PqXixX7SlpOwzLNPOzy4Rm-HvBmHSl8",
+            authDomain: "crunchyroll-clone-c8501.firebaseapp.com",
+            projectId: "crunchyroll-clone-c8501",
+            storageBucket: "crunchyroll-clone-c8501.appspot.com",
+            messagingSenderId: "938254019867",
+            appId: "1:938254019867:web:726750583cc44213ccb7be"
+        }
+
+        this.initapp = initializeApp(this.appconfig);
+        this.auth = getAuth();
+    }
+
+    checkuser = (email, password)=>{
+        signInWithEmailAndPassword(this.auth, email, password).then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        alert("everthing good");
+        // ...
+      }).catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        if(errorCode == "auth/network-request-failed")
+        {
+          alert("check your internet connection");
+        }
+        else
+        {
+          alert("check your email or password");
+    
+        }
+        
+      });
+    }
+
+     createuser = (email, password)=>{
+        createUserWithEmailAndPassword(this.auth, email, password).then((userCredential) => {
+          // Signed in 
+          const user = userCredential.user;
+          alert("account created");
+          // ...
+        }).catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          if(errorCode == "auth/email-already-in-use")
+          {
+            alert("useralready exists");
+          }
+          else if(errorCode == "auth/network-request-failed")
+          {
+            alert("check your internet connection");
+          }
+          // ..
+        });
+      }
+}
+
+
+export {networking};
